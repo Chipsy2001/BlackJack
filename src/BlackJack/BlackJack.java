9e1236d2dc9e38b9501be08d2d04a9bb59ff4738
Chipsy <inas2001@gmail.com>
2024-06-01 14:26:03 +0200

    first commit

---
+++ src/BlackJack/BlackJack.java
@@ -0,0 +1,68 @@
+package BlackJack;
+
+import java.util.Random;
+
+public class BlackJack {
+
+    private static final String SEP = System.lineSeparator();
+
+     private static int[] jLapok=new int[10];
+   private static int[] gLapok=new int[10];
+   private static Random rnd=new Random();
+   
+    public static void main(String[] args) {
+        jatek();
+    }
+
+    private static void jatek() {
+        
+        inicializalas();
+        
+    }
+
+    private static void inicializalas() {
+        kezdoAllapot();
+        konzolraIr("Jatekos lapjai(%d, %d)".formatted(jLapok[0], jLapok[1]));
+        konzolraIr("Gep lapjai(%d, %d)".formatted(gLapok[0], gLapok[1]));
+        huzas();
+    }
+
+    private static int laphuzas() {
+       int huzas=rnd.nextInt(2, 11);
+       return huzas;
+    }
+
+    private static void kezdoAllapot() {
+        for(int i=0; i<2; i++){
+            jLapok[i]=laphuzas();
+            gLapok[i]=laphuzas();
+        }    
+    }
+    
+    private static void huzas() {
+        int kor=2;
+        
+        if(jatekosOsszeg()<15){
+                jLapok[kor]=laphuzas();
+        } else if (jatekosOsszeg()>21){
+            konzolraIr("Jatekos vesztett");
+        } else{
+            konzolraIr("Ker?(I/N)");
+        }
+    
+            
+     }
+     
+    private static void konzolraIr(String kimenet) {
+        System.out.println(kimenet);    
+    }
+
+    private static int jatekosOsszeg() {
+        int osszeg = 0;
+        for (int i = 0; i < jLapok.length; i++) {
+            osszeg +=jLapok[i];
+        }
+        return osszeg;
+    }
+
+}
